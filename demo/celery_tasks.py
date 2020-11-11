27a11dca7415233562cd1116036079fa9b6a22cc
Henrique Brandão <brandao.t.henrique@gmail.com>
2020-11-11 18:32:05 -0300

    checkpoint

--- demo/celery_tasks.py
+++ demo/celery_tasks.py
@@ -1,14 +1,40 @@
 #!/usr/bin/env python3
 
+from loguru import logger
+from celery import Celery
+
+from time import sleep
+from random import randint
 from datetime import datetime
 
-rabbit_broker = ''
-rabbit_port = ''
-rabbit_protocol = ''
+logger.add("log.log", rotation="1 MB")
 
-now = datetime.now().strftime("%H:%M:%S-%m/%d/%Y")
+rb_protocol = 'amqp'
+rb_user = 'arq30'
+rb_pass = 'arq30'
+rb_host = 'coelhomq'
+rb_port = '5672'
+rb_broker = f'{rb_protocol}://{rb_user}:{rb_pass}@{rb_host}:{rb_port}'
 
+
+celery = Celery('fila_guildarq30', broker=rb_broker, backend=rb_protocol)
+
+
+@celery.task()
+def primeira_task(n):
+    logger.info('>> primeira_task')
+    logger.info(f'eu deveria escrever: {n}')
+    sleep(2)
+
+@celery.task()
+def segunda_task(ts):
+    logger.info('>> primeira_task')
+    logger.info(f'eu deveria escrever: {ts}')
+    sleep(2)
 
 
 if __name__ == '__main__':
-    pass
+    numero = randint(1, 100)
+    data = datetime.now().strftime("%H:%M:%S-%m/%d/%Y")
+    primeira_task(numero)
+    segunda_task(timestamp)
--- demo/rabbit_start.sh
+++ demo/rabbit_start.sh
@@ -1,12 +1,15 @@
 echo "# # # Removendo containers anteriores"
-docker container rm -f my-rabbit
+docker container rm -f coelhomq
 
 echo ""
 
-echo "# # # Iniciando container: my-rabbit"
-docker run -d --hostname my-rabbit --name my-rabbit -p 15672:15672 -p 5672:5672 -e RABBITMQ_DEFAULT_USER=guildarq30 -e RABBITMQ_DEFAULT_PASS=guildarq30 rabbitmq:3-management
+echo "# # # Iniciando container: coelhomq"
+docker run -d --hostname coelhomq --name coelhomq \
+    -p 15672:15672 -p 5672:5672 \
+    -e RABBITMQ_DEFAULT_USER=arq30 -e RABBITMQ_DEFAULT_PASS=arq30 \
+    rabbitmq:3-management
 
 echo ""
 
-echo "# # # Logs: my-rabbit"
-docker logs -f my-rabbit
+echo "# # # Logs: coelhomq"
+docker logs -f coelhomq
