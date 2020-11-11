73379287fe7b8c72ba294b6b029fe20ec1f8a1ba
Henrique Brandão <brandao.t.henrique@gmail.com>
2020-11-11 16:39:06 -0300

    checkpoint

---
+++ README.md
@@ -0,0 +1,10 @@
+# Rabbit MQ and Celery
+
+# 1 - Rabbit MQ
+    Multiple messaging protocols queue
+
+# 2 - Celery
+    Task queue, message processor
+
+# 3 - Flower
+    UI for Celery workers
---
+++ celery_tasks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+
+rabbit_broker = ''
+rabbit_port = ''
+rabbit_protocol = ''
+
+now = datetime.now().strftime("%H:%M:%S-%m/%d/%Y")
+
+
+
+if __name__ == '__main__':
+    pass
